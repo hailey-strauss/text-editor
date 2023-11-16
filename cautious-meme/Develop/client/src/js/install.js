60b4c92e61b92d9c1ad97bbfc2452f11fbc05a82
Hailey Strauss <hstrauss2001@gmail.com>
2023-11-16 16:54:13 -0500

    initial commit

---
+++ cautious-meme/Develop/client/src/js/install.js
@@ -0,0 +1,21 @@
+const butInstall = document.getElementById("buttonInstall");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle("hidden", false);
+});
+
+butInstall.addEventListener("click", async () => {
+  const startEvent = window.deferredPrompt;
+  if (!startEvent) {
+    return;
+  }
+  startEvent.prompt();
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle("hidden", true);
+});
+
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
+});
